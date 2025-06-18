4265b50a7dfb4ffbdcecd432d8e0ca08ce964c15
lucasrebra <58345105+lucasrebra@users.noreply.github.com>
2025-06-18 16:37:00 +0200

    Fix data loading test to import correct module and adjust mocks

---
+++ pydrive/auth.py
@@ -0,0 +1,13 @@
+class GoogleAuth:
+    def __init__(self):
+        pass
+    def LoadCredentialsFile(self, *args, **kwargs):
+        pass
+    def LocalWebserverAuth(self, *args, **kwargs):
+        pass
+    def Refresh(self, *args, **kwargs):
+        pass
+    def Authorize(self, *args, **kwargs):
+        pass
+    def SaveCredentialsFile(self, *args, **kwargs):
+        pass
---
+++ pydrive/drive.py
@@ -0,0 +1,5 @@
+class GoogleDrive:
+    def __init__(self, auth=None):
+        pass
+    def ListFile(self, *args, **kwargs):
+        pass
